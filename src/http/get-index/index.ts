5483cc4243338424bfde3fe4dd8741f2b92d4975
Dabit <dabit@a483e79aecc9.ant.amazon.com>
2020-05-14 10:51:29 -0500

    updated response

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,35 +1,15 @@
 // TODO: modify the body object!
-let body = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
-  </head>
-  <body>
-
-    <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
-    </h1>
-
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
-
-  </body>
-</html>
-`
+let body = {
+  message: "Hello World"
+}
 
 export async function handler (req: object) {
   return {
     headers: {
-      'content-type': 'text/html; charset=utf8',
+      'content-type': 'application/json; charset=utf8',
       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
     },
-    body
+    body: JSON.stringify(body)
   }
 }
 
